d0aebe2afdb5bcaea93318eb97dd75229151b68f
hous01 <hous01@vse.cz>
2021-12-09 14:03:52 +0100

    cors to cor, 8000 to 800

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 const express = require('express');
-const cors = require('cors');
+const cors = require('cor');
 
 const app = express();
 app.use(cors());
 
-const port = 8001;
+const port = 800;
 
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'data', 'products.json'));
